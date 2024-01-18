8479e8609915582b27a8e4eac90ba514b757124d
KhiemNguyen15 <khiemsa15@gmail.com>
2024-01-17 21:49:53 -0600

    feat: Set up database

---
+++ todo/database.py
@@ -0,0 +1,55 @@
+import os
+import sqlite3
+
+
+DB_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "tasks.db")
+
+
+def init_database():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    c.execute(
+        """
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task TEXT NOT NULL,
+            due TEXT
+        )
+    """
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def add_task(task, due):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    c.execute("INSERT INTO tasks (task, due) VALUES (?, ?)", (task, due))
+
+    conn.commit()
+    conn.close()
+
+
+def get_tasks():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM tasks")
+    tasks = c.fetchall()
+
+    conn.close()
+
+    return tasks
+
+
+def remove_task(task_id):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    c.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+
+    conn.commit()
+    conn.close()
